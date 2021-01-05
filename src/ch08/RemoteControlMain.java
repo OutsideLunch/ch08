f9e6099ebe6ce2d96c4207e6b5e67db1efb51edc
OutsideLunch <ghrjf94@gmail.com>
2021-01-05 11:29:41 +0900

    익명 구현 객체 사용.

--- src/ch08/RemoteControlMain.java
+++ src/ch08/RemoteControlMain.java
@@ -51,7 +51,56 @@ public class RemoteControlMain {
 		audio2.setMute(false);
 		audio2.setVolume(-10);
 		audio2.turnOff();
-				
+		
+		
+		System.out.println("---- 익명 구현 객체 사용 -----");
+		
+		RemoteControl rc1 = new RemoteControl() {
+			
+		private int volume;
+			
+		public void turnOn() {
+			System.out.println("전원이 켜집니다.");
+		}
+		
+		public void turnOff() {
+			System.out.println("전원이 꺼집니다.");
+		}
+		
+		public void setVolume(int volume) {
+			if (volume > RemoteControl.MAX_VOLUME) {
+				volume = RemoteControl.MAX_VOLUME;
+			}
+			else if (volume < RemoteControl.MIN_VOLUME) {
+				volume = RemoteControl.MIN_VOLUME;
+			}
+			else {
+				this.volume = volume;
+			}
+			System.out.println("현재 Audio 볼륨 : " + this.volume);
+			
+			}
+		};
+		
+//		사용 방법은 동일
+		rc1.turnOn();
+		rc1.setVolume(10);
+		rc1.setMute(true);
+		rc1.turnOff();
+		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
