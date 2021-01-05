18096bc80d3c4f930ef61d200b096fe52b7dab92
OutsideLunch <ghrjf94@gmail.com>
2021-01-05 15:45:14 +0900

    인터페이스의 추상 메서드와 디폴트 메서드의 차이를 확인하기 위한 실행 클래스

---
+++ src/ch08/InterDefaultMain.java
@@ -0,0 +1,13 @@
+package ch08;
+
+public class InterDefaultMain {
+
+	public static void main(String[] args) {
+		
+		InterDefaultImpl idi = new InterDefaultImpl();
+		idi.method1();
+		idi.method2();
+		idi.defaultMethod();
+	}
+
+}
