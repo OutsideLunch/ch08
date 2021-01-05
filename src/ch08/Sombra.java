caedddaa03430cd15c8ec8b07f3056fd1978a339
OutsideLunch <ghrjf94@gmail.com>
2021-01-05 15:46:17 +0900

    인터페이스의 다형성을 이용하여 만든 클래스.

---
+++ src/ch08/Sombra.java
@@ -0,0 +1,40 @@
+package ch08;
+
+public class Sombra implements Character2 {
+
+	@Override
+	public void left() {
+		
+	}
+
+	@Override
+	public void right() {
+		
+	}
+
+	@Override
+	public void forward() {	
+		
+	}
+
+	@Override
+	public void backward() {
+		
+	}
+
+	@Override
+	public void attack() {
+		
+	}
+
+	@Override
+	public void skill() {
+		
+	}
+
+	@Override
+	public void specialSkill() {
+		
+	}
+
+}
